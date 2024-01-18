59cf24ea26a1479ad056062419ef184eeefb6706
Dmitry Pavlov <dmitriiy.pavlov@gmail.com>
2024-01-18 16:58:23 +0300

    add function id facts

--- src/js/main.js
+++ src/js/main.js
@@ -9,55 +9,62 @@ async function requestFilm(url){
 })
 const result = await response.json();
 const allFilms = result.items;
-console.log(allFilms)
-
-
 for (let i = 0; i < allFilms.length; i++) {
-    // const genres = allFilms[i].genres;
-    // let genreNames = '';
-    // for (let j = 0; j < genres.length; j++){
-    //     genreNames+= genres[j].name + ', ';
-    // }
-    // genreNames = genreNames.slice(0, -2);
-    // console.log(genreNames)
-    // console.log(allFilms[i].genres);
+    const id = allFilms[i].kinopoiskId;
+    console.log(id)
     const genreObjects = allFilms[i].genres;
     const genreNames = genreObjects.map(genre => genre.genre).join(', ');
     console.log(genreNames);
-    const filmDetailsHTML = `
+    if (url = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres') {
+        const filmDetailsHTML = `
+        <li class="card__group-item">
+            <img class="card__group-item-img" src="${allFilms[i].posterUrlPreview}" alt="${allFilms[i].nameRu} Poster">
+            <h2 class="card__group-item-head">${allFilms[i].nameRu}</h2>
+            <p class="card__group-item-name">Оригинальное название: ${allFilms[i].nameOriginal}</p>
+            <p class="card__group-item-year">Дата выхода: ${allFilms[i].year}</p>
+            <p class="card__group-item-genr">Жанр: ${genreNames}</p>
+        </li>
+    `;
+    const filmDetailsSection = document.querySelector('.card__group-list');
+    filmDetailsSection.innerHTML += filmDetailsHTML;
+    } else {
+        const filmDetailsHTML = `
         <li class="card__group-item">
             <img class="card__group-item-img" src="${allFilms[i].posterUrlPreview}" alt="${allFilms[i].nameRu} Poster">
             <h2 class="card__group-item-head">${allFilms[i].nameRu}</h2>
             <p class="card__group-item-rating">${allFilms[i].ratingKinopoisk}</p>
-            <p class="card__group-item-name">Original Name: ${allFilms[i].nameOriginal}</p>
-            <p class="card__group-item-year">Year: ${allFilms[i].year}</p>
-            <p class="card__group-item-genr">Genres: ${genreNames}</p>
+            <p class="card__group-item-name">Оригинальное название: ${allFilms[i].nameOriginal}</p>
+            <p class="card__group-item-year">Дата выхода: ${allFilms[i].year}</p>
+            <p class="card__group-item-genr">Жанр: ${genreNames}</p>
         </li>
     `;
-    console.log(genreNames)
     const filmDetailsSection = document.querySelector('.card__group-list');
     filmDetailsSection.innerHTML += filmDetailsHTML;
-
-    const card = document.querySelectorAll('.card__group-item');
-    card.forEach((card) => {
-        card.addEventListener('click', () => {
-        const filmPrevHTML = `
-            <div class="card__group-prev">
-                <h2 class="card__group-item-head">${allFilms[i].nameRu}</h2>
-                <img class="card__group-item-img" src="${allFilms[i].posterUrlPreview}" alt="${allFilms[i].nameRu} Poster">
-                <p class="card__group-item-rating">Rating: ${allFilms[i].ratingKinopoisk}</p>
-                <p class="card__group-item-name">Original Name: ${allFilms[i].nameOriginal}</p>
-                <p class="card__group-item-year">Year: ${allFilms[i].year}</p>
-                <p class="card__group-item-genr">Genres: ${genreNames}</p>
-            </div>
-    `;
-        })
-    })
+    }  
 }
 } catch (error) {
     console.log(`Ошибка при получении данных о клиентах: ${error}`);
 }
 }
+
+// This endpoint returns a list of facts and errors in the movie.
+async function returnFact(id) {
+    try {
+        const response = await fetch(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${id}/facts`,{
+            method: 'GET',
+            headers: {
+                'X-API-KEY': '739ee6ae-b724-4d2d-ac9e-626e5190393f',
+                'Content-Type': 'application/json',
+            }
+        });
+    const result = await response.json();
+    const factArray = result.items;
+    console.log(factArray)
+    } catch {
+
+    }
+}
+returnFact(4468)
 
 requestFilm('https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=1980&month=MAY');
 requestFilm('https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_POPULAR_ALL&page=3')
@@ -77,17 +84,13 @@ films.addEventListener('click', () => {
 premier.addEventListener('click', () => {
     const oldList = document.querySelector('.card__group-list')
     const listNew = document.createElement('ul');
+    
     listNew.classList.add('card__group-list');
     oldList.parentNode.replaceChild(listNew, oldList);
+    
     requestFilm('https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2024&month=JANUARY');
+
 })
-// collect.addEventListener('click', () => {
-//     const oldList = document.querySelector('.card__group-list')
-//     const listNew = document.createElement('ul');
-//     listNew.classList.add('card__group-list');
-//     oldList.parentNode.replaceChild(listNew, oldList);
-//     requestTrailer('https://kinopoiskapiunofficial.tech/api/v2.2/films/468/videos');
-// })
 
 
 
